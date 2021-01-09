430fcede96d82bde8da48dab0cd8bff8d7e623e1
charlie <chentaolu@mail.fcu.edu.tw>
2021-01-09 17:03:01 +0800

    123

--- OOSE_Final_Project/src/DarkChess/Main.java
+++ OOSE_Final_Project/src/DarkChess/Main.java
@@ -12,13 +12,13 @@ public class Main {
         
         ImageIcon board = new ImageIcon(darkBoard.getBoardPath());
         JLabel label1=new JLabel(board);
-        //設定label的位置、大小，label大小為圖片的大小
+
         label1.setBounds(0,0,board.getIconWidth(),board.getIconHeight());
         JFrame frame=new JFrame();
-        //在frame的底層容器新增label
+
         frame.getLayeredPane().add(label1,new Integer(Integer.MIN_VALUE));
 
-        //panelTop，頂層容器
+
         JPanel panelTop=new JPanel();
         panelTop=(JPanel)frame.getContentPane();
 
