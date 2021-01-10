baddfce20c9d235ccf2ed007c68c119c56d6c0d2
YunHaoW <d0786880@mail.fcu.edu.tw>
2021-01-10 22:26:51 +0800

    edit chess class

--- OOSE_Final_Project/src/ChineseChess/ChineseGuardChess.java
+++ OOSE_Final_Project/src/ChineseChess/ChineseGuardChess.java
@@ -6,10 +6,18 @@ import Framework.EatStrategy;
 import Framework.MoveStrategy;
 
 public class ChineseGuardChess extends AbstractGuard {
+  
+  private MoveStrategy moveStrategy;
+  private EatStrategy eatStrategy;
+  private String chessPath = "../OOSE_Final_Project/src/image/";
+  private String redChessPath = "18.png";
+  private String blackChessPath = "2.png";
 
-  public ChineseGuardChess(int x, int y, int priority, boolean Group, boolean status) {
+  public ChineseGuardChess(int x, int y, int priority, boolean Group, boolean status, MoveStrategy moveStrategy, EatStrategy eatStrategy) {
     super(x, y, priority, Group, status);
     // TODO Auto-generated constructor stub
+    setMoveStrategy(moveStrategy);
+    setEatStrategy(eatStrategy);
   }
 
   @Override
@@ -27,25 +35,33 @@ public class ChineseGuardChess extends AbstractGuard {
   @Override
   public MoveStrategy getMoveStrategy() {
     // TODO Auto-generated method stub
-    return null;
+    return this.moveStrategy;
   }
 
   @Override
   public void setMoveStrategy(MoveStrategy moveStrategy) {
     // TODO Auto-generated method stub
-    
+    this.moveStrategy = moveStrategy;
   }
 
   @Override
   public EatStrategy getEatStrategy() {
     // TODO Auto-generated method stub
-    return null;
+    return this.eatStrategy;
   }
 
   @Override
   public void setEatStrategy(EatStrategy eatStrategy) {
     // TODO Auto-generated method stub
-    
+    this.eatStrategy = eatStrategy;
+  }
+  
+  public String getChessPNGPath() {
+    if (super.isGroup()) {
+        return this.chessPath + this.blackChessPath;
+    } else {
+        return this.chessPath + this.redChessPath;
+    }
   }
 
 }
--- OOSE_Final_Project/src/ChineseChess/ChineseHorseChess.java
+++ OOSE_Final_Project/src/ChineseChess/ChineseHorseChess.java
@@ -6,10 +6,18 @@ import Framework.EatStrategy;
 import Framework.MoveStrategy;
 
 public class ChineseHorseChess extends AbstractHorse {
+  
+  private MoveStrategy moveStrategy;
+  private EatStrategy eatStrategy;
+  private String chessPath = "../OOSE_Final_Project/src/image/";
+  private String redChessPath = "24.png";
+  private String blackChessPath = "8.png";
 
-  public ChineseHorseChess(int x, int y, int priority, boolean Group, boolean status) {
+  public ChineseHorseChess(int x, int y, int priority, boolean Group, boolean status, EatStrategy eatStrategy, MoveStrategy moveStrategy) {
     super(x, y, priority, Group, status);
     // TODO Auto-generated constructor stub
+    setMoveStrategy(moveStrategy);
+    setEatStrategy(eatStrategy);
   }
 
   @Override
@@ -27,25 +35,33 @@ public class ChineseHorseChess extends AbstractHorse {
   @Override
   public MoveStrategy getMoveStrategy() {
     // TODO Auto-generated method stub
-    return null;
+    return this.moveStrategy;
   }
 
   @Override
   public void setMoveStrategy(MoveStrategy moveStrategy) {
     // TODO Auto-generated method stub
-    
+    this.moveStrategy = moveStrategy;
   }
 
   @Override
   public EatStrategy getEatStrategy() {
     // TODO Auto-generated method stub
-    return null;
+    return this.eatStrategy;
   }
 
   @Override
   public void setEatStrategy(EatStrategy eatStrategy) {
     // TODO Auto-generated method stub
-    
+    this.eatStrategy = eatStrategy;
+  }
+  
+  public String getChessPNGPath() {
+    if (super.isGroup()) {
+        return this.chessPath + this.blackChessPath;
+    } else {
+        return this.chessPath + this.redChessPath;
+    }
   }
 
 }
